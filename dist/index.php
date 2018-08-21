6a0d505c37cf312f3d2119eec93bcc76d3e5c35c
chazmcgrill <charlietaylordev@gmail.com>
2018-08-21 09:33:16 +0100

    Markup Skeleton

--- dist/index.php
+++ dist/index.php
@@ -4,9 +4,79 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>Restaurant</title>
+  <link rel="stylesheet" href="assets/css/main.css">
 </head>
 <body>
-  <h1>Working</h1>
+  <header>
+    <nav>
+      <div class="social-icons">a b c d</div>
+      <div class="logo">Burgers</div>
+      <div class="nav-menu">menu</div>
+    </nav>
+    
+    <h1>The worlds number one burger</h1>
+    <div class="order-btn">Order</div>
+    <div class="scroll-sign">scroll down</div>
+  </header>
+
+  <section class="section-about">
+    <h2>About us</h2>
+    <p>Mustache hashtag chillwave, DIY gastropub migas cardigan poke hammock chartreuse salvia humblebrag lumbersexual before they sold out. Kale chips iPhone helvetica godard sustainable. Williamsburg seitan chartreuse VHS. Semiotics wayfarers live-edge pok pok. Bitters raclette vexillologist, forage wolf hammock master cleanse cred palo santo pop-up.</p>
+  </section>
+
+  <section class="section-menu">
+    <h2>menu</h2>
+    <div class="menu-item">
+      <h4>Title 1</h4>
+      <img src="" alt="burger 1">
+      <p>helvetica godard sustainable.</p>
+    </div>
+    <div class="menu-item">
+      <h4>Title 2</h4>
+      <img src="" alt="burger 2">
+      <p>helvetica godard sustainable.</p>
+    </div>
+    <div class="menu-item">
+      <h4>Title 3</h4>
+      <img src="" alt="burger 3">
+      <p>helvetica godard sustainable.</p>
+    </div>
+    <div class="full-menu-button"></div>
+  </section>
+
+  <section class="section-reviews">
+    <h2>reviews</h2>
+    <div class="review-box">
+      <div class="review-item">
+        <h5>Hipster</h5>
+        <p>Kale chips iPhone helvetica godard sustainable.</p>
+      </div>
+      <div class="review-item">
+        <h5>Chillwave</h5>
+        <p>DIY gastropub migas cardigan poke hammock chartreuse</p>
+      </div>
+      <div class="review-item">
+        <h5>Wayfarers</h5>
+        <p>Williamsburg seitan chartreuse VHS. Semiotics wayfarers</p>
+      </div>
+    </div>
+  </section>
+
+  <section class="section-find-us">
+    <h2>find us</h2>
+    <div class="find-us-map">
+      map goes here...
+    </div>
+  </section>
+
+  <footer>
+    <nav></nav>
+    <div class="footer-address">
+      <strong>City</strong>
+      <p>123 Street Name, City, PO BOX 123</p>
+    </div>
+  </footer>
+
 </body>
 </html>
