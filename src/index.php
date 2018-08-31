55ed5d76e9bbc13e2ba354d44c1e35f0d2e3bfa5
chazmcgrill <charlietaylordev@gmail.com>
2018-08-31 09:09:36 +0100

    Migrated Layout Styling to New Structure

--- src/index.php
+++ src/index.php
@@ -9,120 +9,127 @@
 </head>
 <body>
   <header>
-    <nav>
-      <div class="container">
-        <div class="social-icons">a b c d</div>
-        <div class="logo">Burgers</div>
-        <div class="nav-menu">menu</div>
-      </div>
+    <nav class="navbar">
+      <div class="navbar--icons">a b c d</div>
+      <div class="navbar--logo">Burgers</div>
+      <div class="navbar--btn">Order Online</div>
     </nav>
     
-    <div class="header-content">
-      <h1>The worlds number one burger</h1>
-      <div class="order-btn">Order</div>
-      <div class="scroll-sign">scroll down</div>
-    </div>
+    <h1>The worlds number one burger</h1>
+    <div class="scroll-sign">scroll down</div>
   </header>
 
-  <section class="section-about">
-    <div class="container">
-      <div class="about-row">
-        <h2>Hello</h2>
-        <p>Mustache hashtag chillwave, DIY gastropub migas cardigan poke hammock chartreuse salvia humblebrag lumbersexual before they sold out. Kale chips iPhone helvetica godard sustainable. Williamsburg seitan chartreuse VHS. Semiotics wayfarers live-edge pok pok. Bitters raclette vexillologist, forage wolf hammock master cleanse cred palo santo pop-up.</p>
+  <section class="about">
+    <div class="row row--spaced">
+      <div class="col">
+        <div class="about-img"></div>
+      </div>
+      <div class="col">
+        <h3>Hello there!</h3>
+        <p class="lead">Mustache hashtag chillwave, DIY gastropub migas cardigan poke hammock chartreuse salvia humblebrag lumbersexual before they sold out. Kale chips iPhone helvetica godard sustainable. Williamsburg seitan chartreuse VHS. Semiotics wayfarers live-edge pok pok. Bitters raclette vexillologist, forage wolf hammock master cleanse cred palo santo pop-up.</p>
         <br/>
         <p>Mixtape copper mug taxidermy, live-edge small batch 3 wolf moon shoreditch keffiyeh direct trade banjo snackwave tofu shabby chic. Narwhal +1 asymmetrical everyday carry chicharrones roof party, mustache forage tote bag man bun.</p>
       </div>
-      <div class="about-row about-img"></div>
     </div>
   </section>
 
-  <section class="section-menu">
-    <h2>Burgers</h2>
-    <div class="container">
-      <div class="menu-item">
-        <h4>Title 1</h4>
-        <p>Readymade hammock tbh next level crucifix mumblecore tattooed, yr hot chicken.</p>
-        <div class="price">£5.99</div>
-      </div>
-      <div class="menu-item">
-        <h4>Title 2</h4>
-        <p>Readymade hammock tbh next level crucifix mumblecore tattooed, yr hot chicken.</p>
-        <div class="price">£5.99</div>
-      </div>
-      <div class="menu-item">
-        <h4>Title 3</h4>
-        <p>Readymade hammock tbh next level crucifix mumblecore tattooed, yr hot chicken.</p>
-        <div class="price">£5.99</div>
+  <section class="menu-section">
+    <div class="row row--center">
+      <div class="col col--wide">
+        <h2>Burgers</h2>
+        <div class="menu">
+          <div class="menu-item">
+            <h4 class="menu-item_title">Title 1</h4>
+            <p class="menu-item_text">Readymade hammock tbh next level crucifix mumblecore tattooed, yr hot chicken.</p>
+            <div class="menu-item_price">£5.99</div>
+          </div>
+          <div class="menu-item">
+            <h4 class="menu-item_title">Title 2</h4>
+            <p class="menu-item_text">Readymade hammock tbh next level crucifix mumblecore tattooed, yr hot chicken.</p>
+            <div class="menu-item_price">£5.99</div>
+          </div>
+          <div class="menu-item">
+            <h4 class="menu-item_title">Title 3</h4>
+            <p class="menu-item_text">Readymade hammock tbh next level crucifix mumblecore tattooed, yr hot chicken.</p>
+            <div class="menu-item_price">£5.99</div>
+          </div>
+        </div>
+        <div class="full-menu-button">full menu</div>
       </div>
     </div>
-    <div class="full-menu-button">full menu</div>
   </section>
 
   <section class="section-reviews">
-    <h2>reviews</h2>
-    <div class="container">
-      <div class="review-box">
-        <div class="review-item">
-          <h5>Hipster</h5>
-          <p>Kale chips iPhone helvetica godard sustainable.</p>
-        </div>
-        <div class="review-item">
-          <h5>Chillwave</h5>
-          <p>DIY gastropub migas cardigan poke hammock chartreuse</p>
-        </div>
-        <div class="review-item">
-          <h5>Wayfarers</h5>
-          <p>Williamsburg seitan chartreuse VHS. Semiotics wayfarers</p>
-        </div>
-        <div class="review-item">
-          <h5>Wayfarers</h5>
-          <p>Williamsburg seitan chartreuse VHS. Semiotics wayfarers</p>
-        </div>
-        <div class="review-item">
-          <h5>Hipster</h5>
-          <p>Kale chips iPhone helvetica godard sustainable.</p>
-        </div>
-        <div class="review-item">
-          <h5>Chillwave</h5>
-          <p>DIY gastropub migas cardigan poke hammock chartreuse</p>
-        </div>
-        <div class="review-item">
-          <h5>Wayfarers</h5>
-          <p>Williamsburg seitan chartreuse VHS. Semiotics wayfarers</p>
-        </div>
-        <div class="review-item">
-          <h5>Wayfarers</h5>
-          <p>Williamsburg seitan chartreuse VHS. Semiotics wayfarers</p>
+    <div class="row row--center">
+      <div class="col col--wide">
+        <h2>Reviews</h2>
+        <div class="reviews">
+          <div class="review">
+            <h5 class="review_user">Hipster</h5>
+            <p class="review_text">Kale chips iPhone helvetica godard sustainable.</p>
+          </div>
+          <div class="review-item">
+            <h5 class="review_user">Chillwave</h5>
+            <p class="review_text">DIY gastropub migas cardigan poke hammock chartreuse</p>
+          </div>
+          <div class="review-item">
+            <h5 class="review_user">Wayfarers</h5>
+            <p class="review_text">Williamsburg seitan chartreuse VHS. Semiotics wayfarers</p>
+          </div>
+          <div class="review-item">
+            <h5 class="review_user">Wayfarers</h5>
+            <p class="review_text">Williamsburg seitan chartreuse VHS. Semiotics wayfarers</p>
+          </div>
+          <div class="review-item">
+            <h5 class="review_user">Hipster</h5>
+            <p class="review_text">Kale chips iPhone helvetica godard sustainable.</p>
+          </div>
+          <div class="review-item">
+            <h5 class="review_user">Chillwave</h5>
+            <p class="review_text">DIY gastropub migas cardigan poke hammock chartreuse</p>
+          </div>
+          <div class="review-item">
+            <h5 class="review_user">Wayfarers</h5>
+            <p class="review_text">Williamsburg seitan chartreuse VHS. Semiotics wayfarers</p>
+          </div>
+          <div class="review-item">
+            <h5 class="review_user">Wayfarers</h5>
+            <p class="review_text">Williamsburg seitan chartreuse VHS. Semiotics wayfarers</p>
+          </div>
         </div>
       </div>
     </div>
   </section>
 
-  <section class="section-find-us">
-    <div class="container">
-      <div class="find-us-box">
+  <section class="section-findus">
+    <div class="row row--center">
+      <div class="col col--wide">
         <h2>find us</h2>
-        <div class="find-us-locations">
-          <div class="find-us-address">
-            <h5>Leeds</h5>
-            <p>ABC Street Name</p>
-            <p>LEEDS</p>
-            <p>LS1 3AB</p>
+        <div class="locations">
+          <div class="locations--address">
+            <h5 class="location--city">Leeds</h5>
+            <p class="location--address">ABC Street Name</p>
+            <p class="location--address">LEEDS</p>
+            <p class="location--address">LS1 3AB</p>
           </div>
-          <div class="find-us-address">
-            <h5>Manchester</h5>
-            <p>ABC Street Name</p>
-            <p>MANCHESTER</p>
-            <p>M1 3AB</p>
+          <div class="locations--address">
+            <h5 class="location--city">Manchester</h5>
+            <p class="location--address">ABC Street Name</p>
+            <p class="location--address">MANCHESTER</p>
+            <p class="location--address">M1 3AB</p>
           </div>
-          <div class="find-us-address">
-            <h5>London</h5>
-            <p>ABC Street Name</p>
-            <p>LONDON</p>
-            <p>E1 3AB</p>
+          <div class="locations--address">
+            <h5 class="location--city">London</h5>
+            <p class="location--address">ABC Street Name</p>
+            <p class="location--address">LONDON</p>
+            <p class="location--address">E1 3AB</p>
           </div>
         </div>
-        <div class="find-us-map">
+      </div>
+    </div>
+    <div class="row row--center">
+      <div class="col col--full">
+        <div class="findus-map">
           map goes here...
         </div>
       </div>
@@ -130,7 +137,7 @@
   </section>
 
   <footer>
-    <div class="container">
+    <div class="row row--spaced">
       <nav>
         <ul>
           <li>about</li>
@@ -139,11 +146,11 @@
         </ul>
       </nav>
       <div class="social-icons">a b c d</div>
-      <div class="footer-address">
-        <h5>Leeds</h5>
-        <p>ABC Street Name</p>
-        <p>LEEDS</p>
-        <p>LS1 3AB</p>
+      <div class="locations-address">
+        <h5 class="location--city">Leeds</h5>
+        <p class="location--address">ABC Street Name</p>
+        <p class="location--address">LEEDS</p>
+        <p class="location--address">LS1 3AB</p>
       </div>
     </div>
   </footer>
