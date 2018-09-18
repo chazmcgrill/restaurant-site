6fa9cbc85b4f69c03689e67d1b1edfeb47c987df
chazmcgrill <charlietaylordev@gmail.com>
2018-09-18 17:22:05 +0100

    Refactored Google Maps Logic / Added Correct Positioning

--- src/js/app.js
+++ src/js/app.js
@@ -15,37 +15,30 @@ orderBtn.forEach(el => {
 // -------- GOOGLE MAPS
 
 function initMap() {
-  
-  var myLatLng = { lat: 53.801277, lng: -1.548567 };
+
+  const mapInfo = [
+    { title: 'Leeds Restaurant', latlng: { lat: 53.801277, lng: -1.548567 } },
+    { title: 'Manchester Restaurant', latlng: { lat: 53.483959, lng: -2.244644 } },
+    { title: 'London Restaurant', latlng: { lat: 51.509865, lng: -0.118092 } },
+  ];
 
   // Create a map object and specify the DOM element for display.
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: myLatLng,
+  const map = new google.maps.Map(document.getElementById('map'), {
+    center: mapInfo[0].latlng,
     zoom: 15,
     styles: mapStyles
   });
 
-  // Create a marker and set its position.
-  var marker = new google.maps.Marker({
-    map: map,
-    position: myLatLng,
-    title: 'Leeds Restaurant'
-  });
+  function createMarker(m) {
+    new google.maps.Marker({
+      map: map,
+      position: m.latlng,
+      title: m.title
+    });
+  }
 
-  var myLatLng2 = { lat: 56.801277, lng: -1.548567 };
+  // Interate through create a marker for items
+  mapInfo.forEach(m => createMarker(m));
 
-  new google.maps.Marker({
-    map: map,
-    position: myLatLng2,
-    title: 'Manchester Restaurant'
-  });
-
-  var myLatLng3 = { lat: 52.801277, lng: -1.548567 };
-
-  new google.maps.Marker({
-    map: map,
-    position: myLatLng3,
-    title: 'London Restaurant'
-  });
 }
 
